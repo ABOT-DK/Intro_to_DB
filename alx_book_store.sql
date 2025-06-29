358d03f5ffd0e512acb55f4a0b79afb310b25346
ABOT-DK <davidkabotsi@gmail.com>
2025-06-29 15:56:26 +0000

    added alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Books (
-    book_id PRIMARY KEY,
+    book_id INT PRIMARY KEY,
     title VARCHAR(130),
     FOREIGN KEY (author_id) REFERENCES Authors(author_id),
     price DOUBLE,
@@ -11,19 +11,19 @@ CREATE TABLE Books (
 )
 
 CREATE TABLE Authors (
-    author_id PRIMARY KEY,
+    author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 )
 
 CREATE TABLE Customers (
-    customer_id PRIMARY KEY,
+    customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 )
 
 CREATE TABLE Orders (
-    order_id PRIMARY KEY,
+    order_id INT PRIMARY KEY,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 )
